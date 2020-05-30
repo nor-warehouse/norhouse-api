40d4b1f6e538b668704480e83e73fc053f866544
Mauro Ocorso <mauro.ocorso@etermax.com>
2020-05-29 23:43:35 -0300

    Adds response type

--- src/core/use_cases/registerPurchase/RegisterPurchaseUseCase.ts
+++ src/core/use_cases/registerPurchase/RegisterPurchaseUseCase.ts
@@ -33,7 +33,7 @@ import {
   RegisterPurchaseRequestDTOSupplier,
 } from './RegisterPurchaseRequestDTO';
 
-export class RegisterPurchaseUseCase implements UseCase<RegisterPurchaseRequestDTO> {
+export class RegisterPurchaseUseCase implements UseCase<RegisterPurchaseRequestDTO, Purchase> {
   constructor(
     private invoicesRepo: InvoicesRepository,
     private suppliersRepo: SuppliersRepository,
