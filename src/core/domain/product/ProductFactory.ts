6431c9ff7ca561e79b65a7bf122233b1429d2a6c
Mauro Ocorso <mauro.ocorso@etermax.com>
2020-06-12 00:44:12 -0300

    Adds ProductFactory

---
+++ src/core/domain/product/ProductFactory.ts
@@ -0,0 +1,35 @@
+import { Factory } from '../../../shared/core/Factory';
+import { UniqueEntityID } from '../../../shared/core/UniqueEntityID';
+import { Category } from './category/Category';
+import { CategoryName } from './category/CategoryName';
+import { Product } from './Product';
+import { ProductName } from './ProductName';
+import { ProductPrice } from './ProductPrice';
+import { ProductStock } from './ProductStock';
+
+interface RawProduct {
+  id?: string;
+  name: string;
+  price: number;
+  stock: number;
+  category: {
+    id?: string;
+    name: string;
+  };
+}
+
+export class ProductFactory implements Factory<Product, RawProduct> {
+  create(raw: RawProduct): Product {
+    const idOrNot = raw.id ? new UniqueEntityID(raw.id) : undefined;
+    const categoryIdOrNot = raw.category.id ? new UniqueEntityID(raw.category.id) : undefined;
+    return Product.create(
+      {
+        category: Category.create({ name: CategoryName.create({ value: raw.category.name }) }, categoryIdOrNot),
+        name: ProductName.create({ value: raw.name }),
+        price: ProductPrice.create({ value: raw.price }),
+        stock: ProductStock.create({ value: raw.stock }),
+      },
+      idOrNot,
+    );
+  }
+}
